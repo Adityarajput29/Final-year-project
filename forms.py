bea5285bb4e20bf950444aa6268cb0f8c04f048f
Adityarajput29 <126917526+Adityarajput29@users.noreply.github.com>
2024-07-25 20:55:47 +0530

    Delete forms.py

--- forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class DocumentForm(forms.Form):
-    document_type = forms.ChoiceField(choices=[('pdf', 'PDF'), ('youtube', 'YouTube Video')])
-    document = forms.FileField(label='Upload Document', required=False)
-    youtube_url = forms.CharField(label='YouTube URL', required=False)
