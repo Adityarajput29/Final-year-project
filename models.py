7777ded9342fda6b04a7b5b821bbbb65e266389d
Adityarajput29 <126917526+Adityarajput29@users.noreply.github.com>
2024-07-25 20:55:32 +0530

    Delete models.py

--- models.py
+++
@@ -1,7 +0,0 @@
-# summarizer/models.py
-from django.db import models
-
-class Document(models.Model):
-    document_type = models.CharField(max_length=20, choices=[('pdf', 'PDF'), ('youtube', 'YouTube')])
-    content = models.TextField()
-    summary = models.TextField(null=True, blank=True)
